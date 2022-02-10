43d2bf1f907d32e085c170ca14d6c2f2a00b4db2
Tyronnosaurus <Tyronnosaurus@protonmail.com>
2022-02-10 18:32:25 +0100

    Refactored javascript filter

--- js/filter.js
+++ js/filter.js
@@ -9,36 +9,14 @@ function filterSelection(c) {
   var items = document.getElementsByClassName("pf-item"); // Get all portfolio items 
 
   for (var i=0; i<items.length; i++) {
-    w3RemoveClass(items[i], "show");                                        // Remove 'show' class
-    if (items[i].className.indexOf(c) > -1) w3AddClass(items[i], "show");   // Reappend it only if it has the tag we want
+    w3.removeClass(items[i], "show");                             // Remove 'show' class
+    if (hasClass(items[i], c)){ w3.addClass(items[i], "show"); }  // Reappend it only if it has the tag we want
   }
 }
 
 
-// Show filtered elements
-function w3AddClass(element, clName) {
-  var i, arr1, arr2;
-  arr1 = element.className.split(" ");
-  arr2 = clName.split(" ");
-  for (i = 0; i < arr2.length; i++) {
-    if (arr1.indexOf(arr2[i]) == -1) {
-      element.className += " " + arr2[i];
-    }
-  }
-}
-
-
-// Hide elements that are not selected
-function w3RemoveClass(element, name) {
-  var i, arr1, arr2;
-  arr1 = element.className.split(" ");
-  arr2 = name.split(" ");
-  for (i = 0; i < arr2.length; i++) {
-    while (arr1.indexOf(arr2[i]) > -1) {
-      arr1.splice(arr1.indexOf(arr2[i]), 1);
-    }
-  }
-  element.className = arr1.join(" ");
+function hasClass(element, c){
+  return (element.className.indexOf(c) > -1);
 }
 
 
