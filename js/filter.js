1edb71d28f42e7b3bfcf54ea6a9c6ac13dd7550a
Tyronnosaurus <Tyronnosaurus@protonmail.com>
2022-02-11 11:50:46 +0100

    Revamped filtering with list, filterHTML

--- js/filter.js
+++ js/filter.js
@@ -1,31 +1,20 @@
-// At start, show all items
-filterSelection("all")
-
-
-// Show/hide portfolio items using the "show" class
-function filterSelection(c) {
-  if (c == "all") c = "";
-
-  var items = document.getElementsByClassName("pf-item"); // Get all portfolio items 
-
-  for (var i=0; i<items.length; i++) {
-    w3.removeClass(items[i], "show");                             // Remove 'show' class
-    if (hasClass(items[i], c)){ w3.addClass(items[i], "show"); }  // Reappend it only if it has the tag we want
-  }
-}
-
-
-function hasClass(element, c){
-  return (element.className.indexOf(c) > -1);
-}
-
-
-// Add active class to the current button (highlight it)
 var btnContainer = document.getElementById("btnContainer");
 var btns = btnContainer.getElementsByClassName("btn");
 
-for (var i = 0; i < btns.length; i++) {
-  
+// Filter according to clicked button
+for (var i=0; i<btns.length; i++) {
+  btns[i].addEventListener(
+    "click",
+    function(){
+      t = this.textContent;
+      if (t=='Show all') t = '';
+      w3.filterHTML('#pf-grid', 'li', t);
+    }
+  );
+}
+
+// Add active class to the clicked button (highlight it)
+for (var i=0; i<btns.length; i++) {
   btns[i].addEventListener(
     "click",
     function(){
@@ -35,5 +24,4 @@ for (var i = 0; i < btns.length; i++) {
       this.className += " active";                                        // Add "active" class to the one we just clicked.
     }
   );
-
 }
